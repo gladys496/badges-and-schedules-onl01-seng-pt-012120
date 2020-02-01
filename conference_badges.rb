28bdc02c7794bc24b9c3e7c4fcda4607c2b4f017
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 00:11:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,6 +2,7 @@ attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz.]
 
 def badge_maker(name)
       "Hello, my name is #{name}."
+    end
 
 def batch_badge_creator(attendees)
   attendees.map do |attendees|
