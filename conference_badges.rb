e96b1556efbeeb1ebe538671fd399b17d140b2d9
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 00:45:27 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,6 +1,6 @@
 attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz.]
 
-def 
+def badge
 def batch_badge_creator(attendees)
   attendees.map do |attendees|
   puts "Hello, my name is #{attendees}!"
