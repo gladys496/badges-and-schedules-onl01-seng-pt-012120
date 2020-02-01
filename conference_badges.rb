9fb67d4659b2a6d0a7ed57c43b0a9e29236341f3
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 01:08:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -8,13 +8,13 @@ end
 
 def batch_badge_creator(attendees)
   attendees.map do |attendee|
-  puts "Hello, my name is #{attendee}!"
+   "Hello, my name is #{attendee}!"
    end 
 end 
 
 def assign_rooms(attendees)
   attendees.each_with_index.map do |attendee, index| 
-  puts "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
+    "Hello, #{attendee}! You'll be assigned to room #{index+1}!"
     end
  end
  
