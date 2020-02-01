8a00659d2670d3d5a3ad65dd701da01e321a95a2
Gladys Gutierrez <gutierrezgladys721@gmail.com>
2020-02-01 00:06:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+attendees = [Edsger, Ada, Charles, Alan, Grace, Linus, Matz.]
+
+def badge_maker(name)
+      "Hello, my name is #
+def batch_badge_creator(attendees)
+  attendees.map do |attendees|
+       "Hello, #{attendees}!"
+   end 
+end 
+
+def assign_rooms(attendees)
+  attendees.each_with_index.map do |attendees, index| 
+        "Hello, #{attendees}! You'll be assigned to room #{index+1}!"
+    end
+ end
+ 
+ def printer(attendees)
+ end 
+  
